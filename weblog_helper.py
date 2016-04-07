731d969c185259b108f398c59fbe0737c3b1d1b7
gary boland <garyboland@gmail.com>
2016-04-07 01:08:49 +0100

    working weblog parser, still needs tests

--- weblog_helper.py
+++ weblog_helper.py
@@ -1,4 +1,5 @@
 import argparse
+from netaddr import IPSet, IPNetwork
 import re
 
 """Parses a standard HTTP access log and outputs log lines that correspond to a
@@ -10,11 +11,14 @@ parser.add_argument('--ip')
 
 def parselog(ip_addr):
   regex = r'^([\d\.)]+)*'
+  ip_addr_set = IPSet(IPNetwork(ip_addr))
+  print 'ip_addr_set %s' % ip_addr_set
   with open('/home/gary/Downloads/public_access.log.txt') as fd:
     for index, line in enumerate(fd):
       match = re.search(regex, line)
-      if match.group() == ip_addr:
+      if match.group() in ip_addr_set:
         print '%s  %s' % (index, line)
+  print 'ip_addr_set len %s' % len(ip_addr_set)
 
 if __name__ == '__main__':
   ip_addr = parser.parse_args().ip
